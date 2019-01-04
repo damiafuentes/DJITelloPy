5c30a6313a4dce3624f18500617a54081d128ca6
damiafuentes <damiafuentes@hotmail.com>
2019-01-04 19:00:24 +0100

    Bug fixed: Reversed image

--- example.py
+++ example.py
@@ -89,6 +89,7 @@ class FrontEnd(object):
             self.screen.fill([0, 0, 0])
             frame = cv2.cvtColor(frame_read.frame, cv2.COLOR_BGR2RGB)
             frame = np.rot90(frame)
+            frame = np.flipud(frame)
             frame = pygame.surfarray.make_surface(frame)
             self.screen.blit(frame, (0, 0))
             pygame.display.update()
