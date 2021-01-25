e2ed09f589216f116c7e4f6fcb417b4ac409cdb6
Jakob Löw <jakob@m4gnus.de>
2021-01-25 13:48:04 +0100

    update setup.py for the djitellopy2 pypi package (ref #68)

--- setup.py
+++ setup.py
@@ -1,30 +1,38 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='djitellopy',  # How you named your package folder (MyLib)
-    packages=['djitellopy'],  # Chose the same as "name"
-    version='1.5',  # Start with a small number and increase it with every change you make
-    license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description='DJI Tello drone python interface using the official Tello SDK including the video stream.',
-    # Give a short description about your library
-    author='Damià Fuentes Escoté',  # Type in your name
-    author_email='damiafuentes@hotmail.com',  # Type in your E-Mail
-    url='https://github.com/damiafuentes',  # Provide either the link to your github or to your website
-    download_url='https://github.com/damiafuentes/TelloSDKPy/archive/v_1.5.tar.gz',  # I explain this later on
-    keywords=['tello', 'dji', 'drone', 'sdk', 'official sdk'],  # Keywords that define your package best
-    install_requires=[  # I get to this in a second
+with open("README.md", "r", encoding="utf-8") as fd:
+    long_description = fd.read()
+
+# replace relative urls to example files with absolute urls to the main git repo
+repo_code_url = "https://github.com/damiafuentes/DJITelloPy/tree/master"
+long_description = long_description.replace("](examples/", "]({}/examples/".format(repo_code_url))
+
+setuptools.setup(
+    name='djitellopy2',
+    packages=['djitellopy2'],
+    version='2.1',
+    license='MIT',
+    description='Tello drone library including support for video streaming, swarms, state packets and more',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jakob Löw',
+    author_email='djitellopy2@m4gnus.de',
+    url='https://github.com/M4GNV5',
+    download_url='https://github.com/damiafuentes/TelloSDKPy/archive/v_2.1.tar.gz',
+    keywords=['tello', 'dji', 'drone', 'sdk', 'official sdk'],
+    install_requires=[
         'numpy',
         'opencv-python',
     ],
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Again, pick a license
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
