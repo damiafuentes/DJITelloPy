fe639495aa238a41d70d8c6d109db230daee9663
Jakob Löw <jakob@m4gnus.de>
2021-06-18 11:43:51 +0200

    fix simple-swarm example trying to fly 0cm with the first drone (closes #94)

--- examples/simple-swarm.py
+++ examples/simple-swarm.py
@@ -13,10 +13,10 @@ swarm.takeoff()
 swarm.move_up(100)
 
 # run by one tello after the other
-swarm.sequential(lambda i, tello: tello.move_forward(i * 20))
+swarm.sequential(lambda i, tello: tello.move_forward(i * 20 + 20))
 
 # making each tello do something unique in parallel
-swarm.parallel(lambda i, tello: tello.move_left(i * 100))
+swarm.parallel(lambda i, tello: tello.move_left(i * 100 + 20))
 
 swarm.land()
 swarm.end()
