4927e3acb89de49412cf5f6cdce5d266d2cc26f7
GitHub <michal.kormanik@gmail.com>
2021-11-15 14:52:30 +0100

    Change request modification

--- examples/panorama/panorama.py
+++ examples/panorama/panorama.py
@@ -1,8 +1,8 @@
-#Simply import of "panorama-module.py" and you can use each function by calling it with name of the drone inside arguments.
+#Simply import of "panoramaModule.py" and you can use each function by calling it with name of the drone inside arguments.
 from djitellopy import Tello
 import cv2
 import time
-import panoramaModuled
+import panoramaModule
 
 
 tello = Tello()
@@ -12,5 +12,5 @@ print(tello.get_battery())
 
 tello.takeoff()
 tello.move_up(500)
-panoramaModuled.panorama_half_clockwise(tello)
+panoramaModule.panorama_half_clockwise(tello)
 tello.land()
